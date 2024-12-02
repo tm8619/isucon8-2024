500bb9efeff861bc76dc1673333e6cdc296d517e
Toshiya Matsuzaki <matsuzaki_toshiya8619@yahoo.co.jp>
2024-12-03 00:09:11 +0900

    Revert "fix"
    This reverts commit d98cc6f1a033330b5b6d68bcdbdcf42ba41226ba.

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -28,7 +28,7 @@ import (
 var eventCache *cache.Cache
 
 func init() {
-	eventCache = cache.New(100*time.Millisecond, 100*time.Millisecond)
+	eventCache = cache.New(500*time.Millisecond, 700*time.Millisecond)
 }
 
 type User struct {
