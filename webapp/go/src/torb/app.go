3a381e30a1677dac2b022034f5326aad11455291
Toshiya Matsuzaki <matsuzaki_toshiya8619@yahoo.co.jp>
2024-12-02 23:16:12 +0900

    fix

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -28,7 +28,7 @@ import (
 var eventCache *cache.Cache
 
 func init() {
-	eventCache = cache.New(100*time.Millisecond, 100*time.Millisecond)
+	eventCache = cache.New(0*time.Millisecond, 100*time.Millisecond)
 }
 
 type User struct {
